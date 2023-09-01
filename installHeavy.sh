f345c0131c9b72c14b1d00b1222e03db83ea8d83
Neill Lewis <zneill@gmail.com>
2023-09-01 17:59:05 -0600

    get better at interpolation hopefully

--- installHeavy.sh
+++ installHeavy.sh
@@ -217,8 +217,8 @@ http {
     access_log /var/log/nginx/access.log;
     error_log /var/log/nginx/error.log;
 
-    ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/${DOMAIN}/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/"$DOMAIN"/privkey.pem;
 
 
     listen 80 ssl;
